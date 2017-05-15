098d9c248ace491e7f1732851ed80ac869c7b55f
Apollo Tang <tang.apollo@gmail.com>
2017-05-14 21:27:19 -0400

    add ChatScreen

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
 
 import {StackNavigator} from 'react-navigation';
@@ -14,12 +15,33 @@ class HomeScreen extends React.Component {
     title: 'Welcome',
   };
   render() {
-    return <Text>Hello, Navigation!</Text>;
+    return (
+      <View>
+        <Text>Hello, Chat App!</Text>
+        <Button
+          onPress={()=>this.props.navigation.navigate('Chat')}
+          title="Chat with Lucy" />
+      </View>
+    );
+  }
+}
+
+class ChatScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Chat with Lucy',
+  };
+  render() {
+    return (
+      <View>
+        <Text>Chat with Lucy</Text>
+      </View>
+    );
   }
 }
 
 const App = StackNavigator({
   Home: { screen: HomeScreen },
+  Chat: { screen: ChatScreen },
 });
 
 export default App;
