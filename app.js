f2269ce7e0564dab3faf524a3ebe592940dc2fab
Apollo Tang <tang.apollo@gmail.com>
2017-05-17 14:08:01 -0400

    expose properties of navigator

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ class HomeScreen extends React.Component {
     return (
       <View>
         <Text>This is content of HomeScreen</Text>
+        <Text>{`this.props: \n ${JSON.stringify(this.props, null, 2)}`}</Text>
+        <Text>{`this.props.keys: \n ${Object.keys(this.props)}`}</Text>
+        <Text>{`screenProps: \n ${this.props.screenProps}`}</Text>
+        <Text>{`navigation.keys: \n ${Object.keys(this.props.navigation)}`}</Text>
         <Button
           onPress={ ()=>this.props.navigation.navigate('StackTwo', {someProp: 'someProp'}) }
           title="Navigate to Stack Two" />
@@ -59,11 +63,25 @@ class ComponentFour extends React.Component {
 };
 
 
-const App = StackNavigator({
+const RootNavigator = StackNavigator({
   Home: { screen: HomeScreen },
   StackTwo: { screen: ComponentWithTabs },
   StackThree: { screen: ComponentThree },
   StackFour: { screen: ComponentFour },
 });
 
+class App extends Component {
+  componentDidMount() {
+    console.log('rootNav: ', this.rootNav)
+  }
+  render() {
+    console.log('App: ', this.props)
+    return (
+      <RootNavigator
+        ref={rootNav=>{
+          this.rootNav = rootNav
+          }} />
+    )
+  }
+}
 export default App;
